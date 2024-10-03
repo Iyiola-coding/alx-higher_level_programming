3ba861fccb533d0fc5d72e0425b17d37cc295bf8
IYITECH <iyiolache@gmail.com>
2024-10-02 18:30:59 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Define a Square class """
+
+
+class Square:
+    """ representation of a Square class """
+
+    def __Init__(self, size):
+        """ Instantation with size with object initialization """
+        self, __size = size
