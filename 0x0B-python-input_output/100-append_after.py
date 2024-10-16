cd074eba762cf44d719b755277a7960d472e1219
IYITECH <iyiolache@gmail.com>
2024-10-16 12:30:12 -0700

    100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,7 +5,7 @@
 def append_after(filename="", search_string="", new_srting=""):
     """append "new_string" after a line containing
     "search_string" in "filename" """
-    with open (filename, 'r', encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         line_list = []
         while True:
             line = f.readline()
