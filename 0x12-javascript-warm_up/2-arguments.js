95a1aec4f8a668106fd8b29e30b681ef248adec5
IYITECH <iyiolache@gmail.com>
2024-12-17 07:19:08 -0800

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 /* script that prints a message depending of the number of arguments passed */
 const args = process.argv;
 
-if (args.lenght <= 2) {
+if (args.length <= 2) {
   console.log('No argument');
-} else if (args.lenght === 3) {
+} else if (args.length === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
