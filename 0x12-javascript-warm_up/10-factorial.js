44c27d2192e449299ba99391b43d8413b6e2afe9
IYITECH <iyiolache@gmail.com>
2024-12-18 04:56:40 -0800

    10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -7,10 +7,10 @@ if (isNaN(args)) {
 }
 
 function factorial (n) {
-  if (n == 1) {
+  if (n === 1) {
     return 1;
   }
-  return n * factorial(n -1);
+  return n * factorial(n - 1);
 }
 
 console.log(factorial(args));
