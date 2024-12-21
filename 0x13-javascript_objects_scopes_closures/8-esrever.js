8010ec893aa67d92e563af57a2b72461017965b1
Iyiola-coding <iyiolache@gmail.com>
2024-12-20 22:04:53 -0800

    8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,12 +2,12 @@
 
 /* Write a function that returns the reversed version of a list */
 exports.esrever = function (list) {
-  let len = list.lenght - 1;
+  let len = list.length - 1;
   let i = 0;
   while ((len - i) > 0) {
     const aux = list[len];
     list[len] = list[i];
-    list[i] = list[i];
+    list[i] = aux;
     i++;
     len--;
   }
--- 0x13-javascript_objects_scopes_closures/8-main.js
+++ 0x13-javascript_objects_scopes_closures/8-main.js
@@ -1,15 +1,6 @@
 #!/usr/bin/node
+const esrever = require('./8-esrever').esrever;
 
-/* Write a function that returns the reversed version of a list */
-exports.esrever = function (list) {
-  let len = list.length - 1;
-  let i = 0;
-  while ((len - i) > 0) {
-    const aux = list[len];
-    list[len] = list[i];
-    list[i] = aux;
-    i++;
-    len--;
-  }
-  return list;
-}
+console.log(esrever([1, 2, 3, 4, 5]));
+console.log(esrever(["School", 89, { id: 12 }, "String"]));
+
